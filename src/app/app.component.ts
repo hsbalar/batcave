08e80f82896688231c02afe64b331c0391c2298e
Hitesh Balar <hsbalar@gmail.com>
2017-01-13 15:38:02 +0530

    App module added

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'batcave-app',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent { }
