9ec6a053cfb7419ecb5e3e5645aeed847807ff26
Hitesh Balar <hsbalar@gmail.com>
2017-01-13 15:42:10 +0530

    Electron main.js files
    - Bootstrap app work
    - Tray feature work

---
+++ main.js
@@ -0,0 +1,52 @@
+const electron = require('electron');
+const tray = require('./tray');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const mainPage = 'file://' + __dirname + '/public/index.html';
+
+var isQuitting = false;
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    autoHideMenuBar: false,
+    webPreferences: {
+      nodeIntegration: false
+    },
+  	width: 1200,
+		height: 750,
+    icon: __dirname + '/resources/app/public/images/batcave_small.png'
+  });
+
+  mainWindow.loadURL(mainPage);
+
+  tray.create(mainWindow);
+
+  mainWindow.openDevTools();
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+  mainWindow.on('close', e => {
+    if (!isQuitting) {
+      e.preventDefault();
+      if (process.platform === 'darwin') {
+        app.hide();
+      } else {
+        mainWindow.hide();
+      }
+    }
+  });
+
+  app.on('before-quit', function() {
+    isQuitting = true;
+  });
+});
---
+++ tray.js
@@ -0,0 +1,43 @@
+const electron = require('electron');
+const path = require('path');
+const app = electron.app;
+let tray = null;
+
+exports.create = function(mainWindow) {
+  if (process.platform === 'darwin' || tray) {
+    return;
+  }
+
+  const iconPath = path.join(__dirname, '/public/images/batcave_small.png');
+
+  const toggleWin = function(){
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  };
+
+  const contextMenu = electron.Menu.buildFromTemplate([
+    {
+      label: 'Restore Batcave',
+      click() {
+        toggleWin();
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click() {
+        app.quit();
+      }
+    }
+  ]);
+
+  tray = new electron.Tray(iconPath);
+  tray.setToolTip('Batcave');
+  tray.setContextMenu(contextMenu);
+  tray.on('click', toggleWin);
+};
